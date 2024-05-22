3598dc11c83624ec9a73dd0a930a33365106d897
Belam Muia <belammuia@maleb.local>
2024-05-22 12:33:08 +0300

    chore: user updates

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:new, :create] 
+  
+    def show
+      @user = User.find(params[:id])
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.create(user_params)
+      if @user.valid? 
+        session[:user_id] = @user.id
+        redirect_to dashboard_path
+      else
+        flash[:error] = "Not a valid username or password"
+        render :new
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+  end
+  
