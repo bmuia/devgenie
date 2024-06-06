2fb67ac2dc07dec1e4db4f62f159cda52121af65
Belam Muia <belammuia@maleb.local>
2024-06-06 18:46:32 +0300

    changes

--- app/controllers/project_owners_controller.rb
+++ app/controllers/project_owners_controller.rb
@@ -1,10 +1,3 @@
 class ProjectOwnersController < ApplicationController
-    include ResourceLoader
 
-
-private
-
-def resource_params
-params.require(:project_owner).permit(:user_id,:project_id)
-end
 end
